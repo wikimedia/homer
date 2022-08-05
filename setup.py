be21d93da466fd7c3dd5396f53832127b7980765
Arzhel Younsi <ayounsi@wikimedia.org>
2022-08-05 18:23:31 +0200

    Bump pynetbox to ~= 6.6
    Good news, according to my local tests it works out of the box.
    
    Bug: T310745
    Change-Id: Ifddf44a161db9cafd77989faa1ff60dcbcdd6245

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ INSTALL_REQUIRES = [
     'Jinja2>=2.10',
     'junos-eznc>=2.2.1,<3',
     'paramiko<=2.8.1',  # T299482
-    'pynetbox>=4.0.6,<=5.3.1',  # pynetbox 6.0.0 introduced backward incompatible changes
+    'pynetbox~= 6.6',
     'pyyaml>=3.11',
     'capirca>=2.0.4',
     'python-dateutil>=2.8.1',
